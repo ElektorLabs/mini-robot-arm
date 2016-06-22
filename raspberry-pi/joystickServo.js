fcf4cf563f5e567e39be2c128c88d4c7eb346d02
René Bohne <rene.bohne@gmail.com>
2016-06-22 17:16:12 +0200

    added four inputs

--- raspberry-pi/joystickServo.js
+++ raspberry-pi/joystickServo.js
@@ -4,12 +4,39 @@ var Gpio = require('pigpio').Gpio,
     pullUpDown: Gpio.PUD_DOWN,
     edge: Gpio.RISING_EDGE
   }),
+  right = new Gpio(17, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_DOWN,
+    edge: Gpio.RISING_EDGE
+  }),
+  up = new Gpio(27, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_DOWN,
+    edge: Gpio.RISING_EDGE
+  }),
+  down = new Gpio(22, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_DOWN,
+    edge: Gpio.RISING_EDGE
+  }),
   led = new Gpio(18, {mode: Gpio.OUTPUT}),
   motor = new Gpio(10, {mode: Gpio.OUTPUT}),
   pulseWidth = 1000,
   increment = 100;
 
-button.on('interrupt', function (level) {
+left.on('interrupt', function (level) {
+  led.digitalWrite(level);
+  motor.servoWrite(pulseWidth+pulseWidth*level);
+});
+right.on('interrupt', function (level) {
+  led.digitalWrite(level);
+  motor.servoWrite(pulseWidth+pulseWidth*level);
+});
+up.on('interrupt', function (level) {
+  led.digitalWrite(level);
+  motor.servoWrite(pulseWidth+pulseWidth*level);
+});
+down.on('interrupt', function (level) {
   led.digitalWrite(level);
   motor.servoWrite(pulseWidth+pulseWidth*level);
 });
